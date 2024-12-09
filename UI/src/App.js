041fac3cc16101741ef42e1bd980a8e987ce8327
mahesh4434 <109942937+mahesh4434@users.noreply.github.com>
2024-12-09 13:33:54 +0530

    Update App.js

--- UI/src/App.js
+++ UI/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
 
     try {
       // Send the request to Flask API with the modified input
-      const response = await axios.post('https://chatbot-deployment-e3gg.vercel.app/process', {
+      const response = await axios.post('https://chatbot-deployment-e3gg.vercel.app', {
         text: modifiedUserInput,
       });
 
